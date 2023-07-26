74f19cd164241bbcc4f1ea1baa7534b2da2bd7b7
mrithunjay26 <mrithunjayshanmuganand@gmail.com>
2023-07-25 22:36:39 -0700

    Mrithunjay fixed frontend and backend connection

--- src/app/demo_backend/page.tsx
+++ src/app/demo_backend/page.tsx
@@ -1,18 +1,25 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function DemoBackend() {
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
 
   useEffect(() => {
-      fetch('/backend/alldata')
-          .then(res => res.json())
-          .then(data => {
-              setMessage("User id from first user: " + data[0].user_id);
+      const headers = {'Access-Control-Allow-Origin': '*'};
+      axios.get('http://127.0.0.1:5000/api/data',{
+      headers: {'Access-Control-Allow-Origin': '*' }
+      }).then(res => {
+              setMessage("User id from first user: " + res.data.message);
               setLoading(false);
           })
+        .catch(error => {
+          // Handle any errors that occurred during the API request
+          console.error('Error:', error.message);
+      });
   }, []);
   return (
     <div>{!loading ? message : "Loading.."}</div>
