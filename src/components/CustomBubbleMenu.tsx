2632a5d89f7e9ae8c86cf8c0a47bb18fa1ce203b
jakob-bjorner <jakob_bjorner@hotmail.com>
2023-07-27 16:01:12 -0700

    add italic to dropdown

--- src/components/CustomBubbleMenu.tsx
+++ src/components/CustomBubbleMenu.tsx
@@ -4,14 +4,17 @@ export default function CustomBubbleMenu(props: { editor: Editor | null }) {
   const setBold = () => props.editor?.chain().focus().toggleBold().run();
   const setItalic = () => props.editor?.chain().focus().toggleItalic().run();
 
-  const style = (mark: string) => "border border-black rounded p-2";
+  const btnStyle = "border border-black rounded p-2";
 
   return (
     <>
       {props.editor ? (
-        <BubbleMenu editor={props.editor}>
-          <button onClick={setBold} className={style("bold")}>
-            Bold
+        <BubbleMenu editor={props.editor} className="flex gap-1">
+          <button onClick={setBold} className={btnStyle}>
+            <strong>Bold</strong>
+          </button>
+          <button onClick={setItalic} className={btnStyle}>
+            <em>Italic</em>
           </button>
         </BubbleMenu>
       ) : undefined}
