a950c98c2923b45ecf9a3f46a7000d8c7aff6a5c
jakob-bjorner <jakob_bjorner@hotmail.com>
2023-07-27 15:34:12 -0700

    correct spelling in comment on backend demo

--- src/app/demo_backend/page.tsx
+++ src/app/demo_backend/page.tsx
@@ -7,7 +7,7 @@ export default function DemoBackend() {
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState(null);
-//an base to work off of to get and post things to the backend
+  //a base to work off of to get and post things to the backend
   useEffect(() => {
       const headers = {'Access-Control-Allow-Origin': '*'};
       axios.get('http://127.0.0.1:5000/api/data',{
