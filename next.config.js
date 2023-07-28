26be2afd44e8c5ad941b32b8393a1df5ffe58815
Pulkit Ahuja <140126488+pulkitahuja0@users.noreply.github.com>
2023-07-27 18:40:26 -0700

    @types/next-auth added

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = () => {
-    const rewrites = () => {
-        return [
-        {
-            source: "/backend/:path*",
-            destination: "http://localhost:5000/v1/:path*",
-        },
-        ];
-    };
-    return {
-        rewrites,
-    };
+  const rewrites = () => {
+    return [
+      {
+        source: "/backend/:path*",
+        destination: "http://localhost:5000/v1/:path*",
+      },
+    ];
+  };
+  return {
+    rewrites,
+  };
 };
-module.exports = nextConfig
+module.exports = nextConfig;
