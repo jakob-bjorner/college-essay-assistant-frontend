1ca42fad1aa3f96adf9446d67821731bc23a719d
SaiLalith Kanumuri <saikanbolt@gmail.com>
2023-08-20 17:25:27 -0700

    Added reply to comment functionality

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,9 +1,47 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
+import { Button } from "reactstrap";
 
 const Comment = (props: PropsWithChildren) => {
+  const [userResponse, setUserResponse] = useState('');
+  const [isInputVisible, setInputVisible] = useState(true);
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      setUserResponse(event.target.value);
+      setInputVisible(false);
+    }
+  };
+  
+  
   return (
+<<<<<<< HEAD
     <div className="dark:bg-gray-700 dark:text-gray-400 bg-gray-200 text-black p-2 rounded-md w-full">
       {props.children}
+=======
+    <div className="text-gray-200 p-2">
+      <div className="dark:bg-gray-700 dark:text-gray-400 bg-white text-black p-2 rounded-md w-full">
+        {props.children}
+      </div>
+      {isInputVisible ? (
+        <div className="dark:bg-gray-700 dark:text-gray-400 bg-white text-black p-2 rounded-md w-full">
+          <input
+            type="text"
+            id="message"
+            name="message"
+            onKeyDown={handleKeyDown}
+          />
+        </div>
+      ) : (
+        <div>
+          <div className="dark:bg-gray-700 dark:text-gray-400 bg-white text-black p-2 rounded-md w-full">
+            {userResponse}
+          </div>
+          <div className="dark:bg-gray-700 dark:text-gray-400 bg-white text-black p-2 rounded-md w-full">
+            Response Loading...
+          </div>
+        </div>
+      )}
+>>>>>>> 19ac1e0 (Added reply to comment functionality)
     </div>
   );
 };
