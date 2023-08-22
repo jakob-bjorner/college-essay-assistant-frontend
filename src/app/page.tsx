2a02ce5d01405c612ede09e20b72eb6e0362cdbe
SaiLalith Kanumuri <saikanbolt@gmail.com>
2023-08-21 18:05:03 -0700

    Implemented comment reply feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { Session } from "next-auth";
 import { useEditor } from "@tiptap/react";
 import type { Editor } from "@tiptap/core";
@@ -16,6 +17,7 @@ import ExtendablePanels from "@/components/ExtendablePanels";
 import ThemeButtons from "@/components/ThemeButtons";
 import type { AppProps } from "next/app";
 // session will be passed into the pageProps: https://stackoverflow.com/questions/73668032/nextauth-type-error-property-session-does-not-exist-on-type
+
 export default function Home() {
   const [comments, setComments] = useState<string[]>([]);
   const onUpdate = ({ editor }: { editor: Editor }) => {
@@ -44,27 +46,33 @@ export default function Home() {
   }, [editor]);
   return (
     <main>
-      <div className="flex ml-16 mr-16 mt-4">
-        {/* <LogInBtn></LogInBtn> */}
+      <div>
+        <div className="cledge-text">
+          cledge.
+        </div>
+        <div className="flex ml-16 mr-16 mt-4">
+          {/* <LogInBtn></LogInBtn> */}
 
-        <ExtendablePanels
-          panel_one={
-            <div className="dark:bg-gray-700 dark:text-gray-400 bg-gray-200 text-black p-2 m-2 rounded-md focus:outline-none max-h-[95vh] overflow-auto">
-              <CustomBubbleMenu editor={editor} />
-              <TipTap editor={editor} />
-            </div>
-          }
-          panel_two={
-            <div className="m-2 grid gap-2">
-              {comments.map((comment, i) => (
-                <Comment key={i}>{comment}</Comment>
-              ))}
-            </div>
-          }
-          unique_panel_id={"panel_one"}
-          panel_one_width={100}
-        />
+          <ExtendablePanels
+            panel_one={
+              <div className="dark:bg-gray-700 dark:text-gray-400 bg-gray-200 text-black p-2 m-2 rounded-md focus:outline-none max-h-[95vh] overflow-auto">
+                <CustomBubbleMenu editor={editor} />
+                <TipTap editor={editor} />
+              </div>
+            }
+            panel_two={
+              <div className="m-2 grid gap-2">
+                {comments.map((comment, i) => (
+                  <Comment key={i}>{comment}</Comment>
+                ))}
+              </div>
+            }
+            unique_panel_id={"panel_one"}
+            panel_one_width={100}
+          />
+        </div>
       </div>
+      
       <ThemeButtons></ThemeButtons>
       {/* <Link href="/demo_backend">Go to demo backend</Link> */}
     </main>
