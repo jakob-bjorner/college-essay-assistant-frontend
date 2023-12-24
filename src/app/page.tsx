84627954eb6e5a33729ed57fd1a4b55e37ba81dd
jakob-bjorner <jakob_bjorner@hotmail.com>
2023-12-24 10:58:58 -0800

    edit example comment for clarity for users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,23 +50,23 @@ export default function Home() {
     [
       {
         id: "xxijiji",
-        text: "This is a sample comment.",
+        text: 'Example comment on the text: "For one, an opportunity to meet", this section is well writen but if ever more content needs to be added, "For one" can be omitted.',
         author: "AI",
         timestamp: new Date(2001, 8, 27),
         essaySectionReference: "Text",
         versionOfEssay:
-          "Prompt: What historical moment or event do you wish you could have witnessed? (50 words max)\n\nThe Trinity test, the first detonation of the atomic bomb. For one, an opportunity to meet my role models: Oppenheimer, Feynman, Fermi, etc. But also, to witness the 4 millisecond shift to an era of humanity that could eradicate itself. “Now I am become Death, the destroyer of worlds.” Text",
+          "Prompt: What historical moment or event do you wish you could have witnessed? (50 words max)\n\nThe Trinity test, the first detonation of the atomic bomb. For one, an opportunity to meet my role models: Oppenheimer, Feynman, Fermi, etc. But also, to witness the 4 millisecond shift to an era of humanity that could eradicate itself. “Now I am become Death, the destroyer of worlds.”",
       },
     ],
     [
       {
         id: "xxijiji",
-        text: "This is a sample comment.",
+        text: 'Example comment on the text: "For one, an opportunity to meet", this section is well writen but if ever more content needs to be added, "For one" can be omitted.',
         author: "AI",
         timestamp: new Date(2001, 8, 27),
         essaySectionReference: "Text",
         versionOfEssay:
-          "Prompt: What historical moment or event do you wish you could have witnessed? (50 words max)\n\nThe Trinity test, the first detonation of the atomic bomb. For one, an opportunity to meet my role models: Oppenheimer, Feynman, Fermi, etc. But also, to witness the 4 millisecond shift to an era of humanity that could eradicate itself. “Now I am become Death, the destroyer of worlds.” Text",
+          "Prompt: What historical moment or event do you wish you could have witnessed? (50 words max)\n\nThe Trinity test, the first detonation of the atomic bomb. For one, an opportunity to meet my role models: Oppenheimer, Feynman, Fermi, etc. But also, to witness the 4 millisecond shift to an era of humanity that could eradicate itself. “Now I am become Death, the destroyer of worlds.”",
       },
     ],
   ]);
--- src/examplecontent.js
+++ src/examplecontent.js
@@ -18,25 +18,25 @@ const json = {
       content: [
         {
           type: "text",
-          text: `The Trinity test, the first detonation of the atomic bomb. For one, an opportunity to meet my role models: Oppenheimer, Feynman, Fermi, etc. But also, to witness the 4 millisecond shift to an era of humanity that could eradicate itself. “Now I am become Death, the destroyer of worlds.” `,
+          text: `The Trinity test, the first detonation of the atomic bomb.`,
         },
-      ],
-    },
-    {
-      type: "paragraph",
-      content: [
         {
           type: "text",
           marks: [
             {
               type: "comment",
               attrs: {
-                comment: "Example comment text",
+                comment:
+                  'Example comment text on the text: "For one, an opportunity to meet", this section is well writen but if ever more content needs to be added, "For one" can be omitted',
                 commentId: "xxijiji",
               },
             },
           ],
-          text: "Text",
+          text: " For one, an opportunity to meet",
+        },
+        {
+          type: "text",
+          text: ` my role models: Oppenheimer, Feynman, Fermi, etc. But also, to witness the 4 millisecond shift to an era of humanity that could eradicate itself. “Now I am become Death, the destroyer of worlds.” `,
         },
       ],
     },
